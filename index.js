73ed345add32283059bda4d42c0d3a1545f9f0ac
tuii999898 <magarobloxpun0suy@hotmail.com>
2024-05-10 17:19:24 +0700

    update commit

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-function numIslands(grid) {
-  if (!grid || grid.length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(grid, i, j);
-      }
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
     }
   }
-  return islands;
-}
-function dfs(grid, i, j) {
-  const m = grid.length;
-  const n = grid[0].length;
-  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
 }
