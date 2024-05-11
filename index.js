ca1b010e984bf1f4913ef209013327952943a1e4
tuii999898 <magarobloxpun0suy@hotmail.com>
2024-05-11 19:49:39 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-  return prev;
-}
+console.log("hello world");
